90971fd4119f3cd254747d4147b774ee31f4666e
reisdev <matheusdrdj@gmail.com>
2020-03-31 21:58:34 -0300

    [refactor] (post): Changing post slug separator from "_" to "-"

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,32 +1,32 @@
-const postBase = require('../models/SchemaPost')
-const adminBase = require('../models/SchemaAdmin')
+const postBase = require("../models/SchemaPost");
+const adminBase = require("../models/SchemaAdmin");
 
 module.exports = {
   async index(req, res) {
-    const data = await postBase.find()
-    res.json(data)
+    const data = await postBase.find();
+    res.json(data);
   },
   async show(req, res) {
-    let { title } = req.params
-    title = title.replace(/_/g, ' ')
+    let { title } = req.params;
+    title = title.replace(/-/g, " ");
 
-    const post = await postBase.findOne({ title })
-    if (!post) res.status(404).json({ 'message': 'Post não encontrado' })
+    const post = await postBase.findOne({ title });
+    if (!post) res.status(404).json({ message: "Post não encontrado" });
 
-    res.json(post)
+    res.json(post);
   },
 
   async store(req, res) {
     const { title, description, author, id } = req.body;
-    const authauthor = await adminBase.find({ _id: id, name: author })
+    const authauthor = await adminBase.find({ _id: id, name: author });
     if (!authauthor) {
-      res.status(401).send("Author não é valido")
+      res.status(401).send("Author não é valido");
     }
     await postBase.create({
       title,
       description,
       author
-    })
-    res.json({ title, description, author })
+    });
+    res.json({ title, description, author });
   }
-}
+};
